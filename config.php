e8486a57f81c34643c4f1df22960426cd80fba94
Administrator <Administrator@USER-20150709EQ>
2015-07-23 17:25:52 +0800

    配置文件

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php 
+return  array (
+  'DB_TYPE' => 'mysql',
+  'DB_HOST' => 'localhost',
+  'DB_NAME' => 'test',
+  'DB_USER' => 'root',
+  'DB_PWD' => '123456',
+  'DB_PORT' => '3306',
+  'DB_PREFIX' => 'ey_',
+  //'install' => 1,
+);
